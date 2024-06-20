c60131f50bc522ec6b1271176d529354aaba4ddb
Nikola <nikoladjunisic@gmail.com>
2024-06-20 09:51:03 +0200

    Added layout, home, nav and footer component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      // {
+      //   path: 'tracks',
+      //   loadChildren: () =>
+      //     import('./components/tracks/tracks.module').then(
+      //       (m) => m.RecipesModule
+      //     ),
+      // },
+      // {
+      //   path: 'shopping-list',
+      //   loadChildren: () =>
+      //     import('./components/shopping-list/shopping-list.module').then(
+      //       (m) => m.ShoppingListModule
+      //     ),
+      // },
+      // { path: 'contact-us', component: ContactUsComponent },
+      // { path: 'author', component: AuthorComponent },
+      // { path: 'login', component: LoginComponent },
+      // { path: 'register', component: RegisterComponent },
+      // {
+      //   path: 'admin',
+      //   component: AdminLayoutComponent,
+      //   canActivateChild: [adminGuard],
+      //   loadChildren: () =>
+      //     import('./components/admin/admin.module').then((m) => m.AdminModule),
+      // },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NavComponent } from './components/fixed/nav/nav.component';
+import { FooterComponent } from './components/fixed/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    NavComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
