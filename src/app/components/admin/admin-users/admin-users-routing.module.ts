6bfb735a0a1d98d9da5a20080137dbeb8b2f94c9
Nikola-02 <nikoladjunisic@gmail.com>
2024-06-25 13:16:09 +0200

    Updated paths

--- src/app/components/admin/admin-users/admin-users-routing.module.ts
+++ src/app/components/admin/admin-users/admin-users-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminUsersDashboardComponent } from './admin-users-dashboard/admin-users-dashboard.component';
+import { AdminUsersCreateComponent } from './admin-users-create/admin-users-create.component';
+import { AdminUsersEditComponent } from './admin-users-edit/admin-users-edit.component';
 
 const routes: Routes = [
   { path: '', component: AdminUsersDashboardComponent },
