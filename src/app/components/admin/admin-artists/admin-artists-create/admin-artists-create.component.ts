4a432f3dd621faddec5040a9c618a044ed9e7e6b
Nikola-02 <nikoladjunisic@gmail.com>
2024-06-26 12:00:27 +0200

    Added create for artists admin panel

--- src/app/components/admin/admin-artists/admin-artists-create/admin-artists-create.component.ts
+++ src/app/components/admin/admin-artists/admin-artists-create/admin-artists-create.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { AdminArtistsService } from '../admin-artists.service';
+import { PopUpService } from '../../../../shared/services/pop-up.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-artists-create',
   templateUrl: './admin-artists-create.component.html',
-  styleUrl: './admin-artists-create.component.scss'
+  styleUrl: './admin-artists-create.component.scss',
 })
-export class AdminArtistsCreateComponent {
+export class AdminArtistsCreateComponent implements OnDestroy {
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+  });
 
+  public validationErrors;
+
+  public createArtistSub: Subscription;
+
+  constructor(
+    private adminArtistsService: AdminArtistsService,
+    private popUpService: PopUpService,
+    private router: Router
+  ) {}
+
+  onCreateNewArtist() {
+    if (this.form.valid) {
+      let formData = {
+        name: this.form.get('name')?.value,
+      };
+
+      this.createArtistSub = this.adminArtistsService
+        .createNewArtist(formData)
+        .subscribe({
+          next: (response) => {
+            this.popUpService.show(
+              'Successfully added artist.',
+              'success-snack-bar'
+            );
+
+            this.router.navigate(['/admin/artists']);
+          },
+          error: (error) => {
+            console.log(error);
+
+            if (error.status == 401) {
+              this.popUpService.show(
+                'You dont have permission to do this action.',
+                'error-snack-bar'
+              );
+              return;
+            }
+
+            if (error.status == 422) {
+              this.validationErrors = error.error;
+              return;
+            }
+
+            if (error.status == 500) {
+              this.popUpService.show(
+                'Error occured while adding artist.',
+                'error-snack-bar'
+              );
+              return;
+            }
+
+            this.popUpService.show(
+              'Error occured while adding artist.',
+              'error-snack-bar'
+            );
+          },
+        });
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.createArtistSub) {
+      this.createArtistSub.unsubscribe();
+    }
+  }
 }
--- src/app/components/admin/admin-artists/admin-artists.module.ts
+++ src/app/components/admin/admin-artists/admin-artists.module.ts
@@ -5,17 +5,14 @@ import { AdminArtistsRoutingModule } from './admin-artists-routing.module';
 import { AdminArtistsCreateComponent } from './admin-artists-create/admin-artists-create.component';
 import { AdminArtistsEditComponent } from './admin-artists-edit/admin-artists-edit.component';
 import { AdminArtistsDashboardComponent } from './admin-artists-dashboard/admin-artists-dashboard.component';
-
+import { SharedModule } from '../../../shared/shared.module';
 
 @NgModule({
   declarations: [
     AdminArtistsCreateComponent,
     AdminArtistsEditComponent,
-    AdminArtistsDashboardComponent
+    AdminArtistsDashboardComponent,
   ],
-  imports: [
-    CommonModule,
-    AdminArtistsRoutingModule
-  ]
+  imports: [CommonModule, AdminArtistsRoutingModule, SharedModule],
 })
-export class AdminArtistsModule { }
+export class AdminArtistsModule {}
